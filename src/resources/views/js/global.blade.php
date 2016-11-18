f50640136871c5d55473e617d92e6ce56c624258
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2016-11-18 16:53:50 +0200

    Added Global js/css blade

---
+++ src/resources/views/js/global.blade.php
@@ -0,0 +1,2 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
