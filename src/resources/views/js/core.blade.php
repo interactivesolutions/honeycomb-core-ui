f729c7bdc720b8b628dfc2793df34fe36d4942bb
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2016-11-18 17:39:08 +0200

    Added HC Objects class

--- src/resources/views/js/core.blade.php
+++ src/resources/views/js/core.blade.php
@@ -5,6 +5,7 @@
 
     Minify::javascript([
         '/js/shared/hc-helpers.js',
+        '/js/shared/hc-objects.js',
         '/js/shared/hc-functions.js',
         '/js/shared/hc-loader.js',
     ])
