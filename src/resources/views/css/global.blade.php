29f082c848e11b788543f347f50e506524f8ca2a
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2016-11-19 15:58:55 +0200

    Included font awesome

--- src/resources/views/css/global.blade.php
+++ src/resources/views/css/global.blade.php
@@ -6,6 +6,7 @@
         'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.5/css/bootstrap-grid.min.css',
         'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.5/css/bootstrap-reboot.min.css',
         'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.5/css/bootstrap.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
     ])
 
 !!}
