fb52f9db77a01d6decf1585ce7d1e606068f7012
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2016-11-21 19:00:39 +0200

    Refactoring continues

---
+++ src/resources/views/js/form.blade.php
@@ -0,0 +1,20 @@
+{!!
+
+    Minify::javascript([
+        '/js/shared/hc-form-manager.js',
+        '/js/shared/hc-form.js',
+
+        '/js/form/hc-form-basic-field.js',
+        '/js/form/hc-form-button.js',
+        '/js/form/hc-form-single-line.js',
+        '/js/form/hc-form-email.js',
+        '/js/form/hc-form-password.js',
+        '/js/form/hc-form-date-time-picker.js',
+        '/js/form/hc-form-text-area.js',
+        '/js/form/hc-form-rich-text-area.js',
+        '/js/form/hc-form-check-box-list.js',
+        '/js/form/hc-form-radio-list.js',
+        '/js/form/hc-form-drop-down-list.js',
+    ])
+
+!!}
---
+++ src/resources/views/js/shared.blade.php
@@ -0,0 +1,11 @@
+{!!
+
+    Minify::javascript([
+        '/js/shared/hc-helpers.js',
+        '/js/shared/hc-objects.js',
+        '/js/shared/hc-functions.js',
+        '/js/shared/hc-loader.js',
+        '/js/shared/hc-service.js',
+    ])
+
+!!}
