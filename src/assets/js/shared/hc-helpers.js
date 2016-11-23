832188b1db16aa9b515391c9a06e1a2c261f42b8
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2016-11-23 15:07:40 +0200

    Added new helper function Array.remove

--- src/assets/js/shared/hc-helpers.js
+++ src/assets/js/shared/hc-helpers.js
@@ -27,3 +27,21 @@ jQuery.fn.outerHTML = function (s)
         ? this.before (s).remove ()
         : jQuery ("<p>").append (this.eq (0).clone ()).html ();
 };
+
+/**
+ * Removing item from array by value
+ *
+ * @credit http://stackoverflow.com/a/3955096/657451
+ *
+ * @returns {Array}
+ */
+Array.prototype.remove = function() {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
