3c8ddafda8e0351e926c211e2d59b1855bdf711a
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2016-11-26 17:02:38 +0200

    Added jquery ui functionality

--- src/resources/views/js/global.blade.php
+++ src/resources/views/js/global.blade.php
@@ -9,7 +9,8 @@
         'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.43/js/bootstrap-datetimepicker.min.js',
         'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js',
         'https://cdnjs.cloudflare.com/ajax/libs/tinymce/4.5.0/tinymce.min.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.8/js/app.min.js'
+        'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.8/js/app.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js'
     ])
 
 !!}
