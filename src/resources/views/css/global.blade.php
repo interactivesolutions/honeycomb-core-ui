44eb57f850e01154a6de8112582e02213cad7541
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-06-03 09:13:57 +0300

    Moved back to bootstrap 3.3.7

--- src/resources/views/css/global.blade.php
+++ src/resources/views/css/global.blade.php
@@ -2,10 +2,7 @@
 
     Minify::stylesheet([
         'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.css',
-        'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.5/css/bootstrap-flex.min.css',
-        'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.5/css/bootstrap-grid.min.css',
-        'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.5/css/bootstrap-reboot.min.css',
-        'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.5/css/bootstrap.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css',
         'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
         'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.43/css/bootstrap-datetimepicker.min.css',
         'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css',
