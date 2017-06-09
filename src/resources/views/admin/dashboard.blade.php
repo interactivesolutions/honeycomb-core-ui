8b4cba6845393e548a257d9e3bade1ad213d3656
Mantas Paulauskas <mantopastas@gmail.com>
2017-06-09 11:20:38 +0300

    updated admin dashboard view

--- src/resources/views/admin/dashboard.blade.php
+++ src/resources/views/admin/dashboard.blade.php
@@ -9,7 +9,7 @@
 
                 <div class="info-box-content">
                     <span class="info-box-text">{{ trans('users::dash.users') }}</span>
-                    <span class="info-box-number">{{ \interaktyvussprendimai\ocv3users\models\OCUsers::count() }}</span>
+                    <span class="info-box-number">{{ \interactivesolutions\honeycombacl\app\models\HCUsers::count() }}</span>
                 </div>
                 <!-- /.info-box-content -->
             </div>
@@ -17,34 +17,6 @@
         </div>
         <!-- /.col -->
 
-        @if(isPackageEnabled(\interaktyvussprendimai\camovies\providers\CAMoviesServiceProvider::class))
-            <div class="col-md-3 col-sm-6 col-xs-12">
-                <div class="info-box">
-                    <span class="info-box-icon bg-yellow"><i class="ion ion-ios-film"></i></span>
 
-                    <div class="info-box-content">
-                        <span class="info-box-text">{{ trans('camovies::movies.dash.movies') }}</span>
-                        <span class="info-box-number">{{ \interaktyvussprendimai\camovies\models\CAMovies::count() }}</span>
-                    </div>
-                    <!-- /.info-box-content -->
-                </div>
-                <!-- /.info-box -->
-            </div>
-            <!-- /.col -->
-
-            <div class="col-md-3 col-sm-6 col-xs-12">
-                <div class="info-box">
-                    <span class="info-box-icon bg-yellow"><i class="ion ion-ios-people"></i></span>
-
-                    <div class="info-box-content">
-                        <span class="info-box-text">{{ trans('camovies::movies.dash.persons') }}</span>
-                        <span class="info-box-number">{{ \interaktyvussprendimai\camovies\models\CAPersons::count() }}</span>
-                    </div>
-                    <!-- /.info-box-content -->
-                </div>
-                <!-- /.info-box -->
-            </div>
-            <!-- /.col -->
-        @endif
     </div>
 @endsection
