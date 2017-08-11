b1bd80af2b68c752d207e444fd7298ae981287e8
Mantas Paulauskas <mantopastas@gmail.com>
2017-08-11 10:56:47 +0300

    updated libraries versions

--- src/resources/views/css/global.blade.php
+++ src/resources/views/css/global.blade.php
@@ -4,9 +4,9 @@
         'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.css',
         'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css',
         'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
-        'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.43/css/bootstrap-datetimepicker.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css',
         'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css',
-        'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.8/css/AdminLTE.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.11/css/AdminLTE.min.css',
         'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.8/css/skins/_all-skins.min.css'
     ])
 
--- src/resources/views/js/global.blade.php
+++ src/resources/views/js/global.blade.php
@@ -6,10 +6,10 @@
         'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js',
         'https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js',
         'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.5/js/bootstrap.min.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.43/js/bootstrap-datetimepicker.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js',
         'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/tinymce/4.5.0/tinymce.min.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.8/js/app.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/tinymce/4.6.5/tinymce.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.11/js/app.min.js',
         'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js'
     ])
 
